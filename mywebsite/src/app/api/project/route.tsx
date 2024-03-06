d26969b01b84ce0b7440eb2316fbd31ece6ffadf
Nicola Boscolo <nicolaboscolocegion@gmail.com>
2024-03-06 20:49:52 +0100

    get routes

---
+++ mywebsite/src/app/api/project/route.tsx
@@ -0,0 +1,28 @@
+"use server";
+
+import { NextRequest, NextResponse } from 'next/server';
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('http://localhost:8000', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE')
+
+
+export async function GET(req: NextRequest) {
+
+  const url = new URL(req.url);
+
+  //res.status(200).json({ message: 'Hello from Next.js!' })
+  try {
+    // Fetch data from Supabase
+    const { data: education, error } = await supabase
+      .from('projects')
+      .select('*') // Or select specific columns
+
+    if (error) {
+      throw error;
+    }
+
+    return NextResponse.json(education, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+
+}
--- mywebsite/src/app/components/card.tsx
+++ mywebsite/src/app/components/card.tsx
@@ -32,7 +32,8 @@ export function Card(props: Props) {
           {props.description}
         </p>
 
-        <div  className="pt-5">
+        <div className="pt-5">
+        <a href="/api/education">
           <button className="p-[3px] relative">
             <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
             <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
@@ -40,6 +41,7 @@ export function Card(props: Props) {
             </div>
 
           </button>
+        </a>
         </div>
       </BackgroundGradient>
     </div >
