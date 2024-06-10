8b4262d1975eccc1dc75098ae4e068b64455cde4
Nicola <nicolaboscolocegion@gmail.com>
2024-06-10 13:42:28 +0200

    fix:typo

--- mywebsite/src/app/jobs/page.tsx
+++ mywebsite/src/app/jobs/page.tsx
@@ -28,7 +28,7 @@ export default async function Jobs() {
         alignItems="center">
 
         {jobs.map((job: WorkContent, index: number) =>
-          <Grid key={index} className='m-20'>
+          <Grid item key={index} className='m-20'>
             <Card title={job.name} description={job.description} link={job.link} image={job.image} />
 
           </Grid>
